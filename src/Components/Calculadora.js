472d0a4a29aa13f08415ee9bdca2b78afc64a49c
luiz <luizmarcelolm@hotmail.com>
2022-10-15 17:31:49 -0300

    config dos operadores

--- src/Components/Calculadora.js
+++ src/Components/Calculadora.js
@@ -8,7 +8,11 @@ import {Box} from '@mui/material';
 export default function Calculadora(){
 
 const [numero,setNumero] = useState(0);
+const [antigoNumero,setAntigoNumero] = useState(0);
+const [operador,setOperador] = useState();
 
+
+//tira zero do input e subtitui por numero digitado
 function inputNumero(e){
     var input = e.target.value
     if(numero===0)
@@ -18,14 +22,17 @@ function inputNumero(e){
     }
 }
 
+//limpa input
 function limpar(){
     setNumero(0)
 }
 
+//calculo de porcentagem
 function porcento(){
     setNumero(numero/100);
 }
 
+//muda sinal
 function sinal(){
     if(numero > 0){
         setNumero(-numero);
@@ -33,6 +40,29 @@ function sinal(){
             setNumero(Math.abs(numero));
         }
 }
+
+//guarda operação selecionada
+function operacaoHandle(e){
+    var operadorInput=e.target.value;
+    setOperador(operadorInput);
+    setAntigoNumero(numero);
+    setNumero(0);
+}
+
+//calcula os numeros digitados
+function calcular(){
+   if (operador === "/"){
+    setNumero(parseFloat(antigoNumero) / parseFloat(numero))
+   }else if (operador === "x"){
+        setNumero(parseFloat(antigoNumero) * parseFloat(numero))
+   }else  if (operador === "-"){
+    setNumero(parseFloat(antigoNumero) - parseFloat(numero))
+   }else if (operador === "+"){
+        setNumero(parseFloat(antigoNumero) + parseFloat(numero));
+   }
+}
+
+//deleta ultimo numero digitado
 function del(e){
 var input = e.target.value
 setNumero = input.substring(0,input.length-1);
@@ -49,23 +79,23 @@ setNumero = input.substring(0,input.length-1);
                 <button className="red" onClick={limpar}>AC</button>
                 <button className="red" onClick={del}>Del</button>
                 <button className="yellow" onClick={porcento}>%</button>
-                <button className="yellow">/</button>
+                <button className="yellow" onClick={operacaoHandle} value="/">/</button>
                 <button className="blue" onClick={inputNumero} value={7}>7</button>
                 <button className="blue" onClick={inputNumero} value={8}>8</button>
                 <button className="blue" onClick={inputNumero} value={9}>9</button>
-                <button className="yellow">x</button>
+                <button className="yellow"onClick={operacaoHandle} value="x">x</button>
                 <button className="blue" onClick={inputNumero} value={4}>4</button>
                 <button className="blue" onClick={inputNumero} value={5}>5</button>
                 <button className="blue" onClick={inputNumero} value={6}>6</button>
-                <button className="yellow">+</button>
+                <button className="yellow" onClick={operacaoHandle} value="+">+</button>
                 <button className="blue" onClick={inputNumero} value={1}>1</button>
                 <button className="blue" onClick={inputNumero} value={2}>2</button>
                 <button className="blue" onClick={inputNumero} value={3}>3</button>
-                <button className="yellow">-</button>
+                <button className="yellow" onClick={operacaoHandle} value="-">-</button>
                 <button className="green" onClick={sinal}>+/-</button>
                 <button className="blue" onClick={inputNumero} value={0}>0</button>
-                <button className="green" onClick={inputNumero} value=",">,</button>
-                <button className="green">=</button>
+                <button className="green" onClick={inputNumero} value=".">,</button>
+                <button className="green" onClick={calcular} >=</button>
                 </div>
             </Container>
         </div>
